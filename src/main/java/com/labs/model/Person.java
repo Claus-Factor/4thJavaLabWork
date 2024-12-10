1c5c4a5f9b9fb8ae07ed8a36b2b42b8116d71279
Claus-Factor <volxin.k2004@gmail.com>
2024-12-10 16:57:43 +0300

    Add entity Person

---
+++ src/main/java/com/labs/model/Person.java
@@ -0,0 +1,81 @@
+package com.labs.model;
+
+import java.time.LocalDate;
+
+public class Person {
+    private int id;
+    private String name;
+    private char gender;
+    private Department department;
+    private double salary;
+    private LocalDate birthDate;
+
+    public Person(int id, String name, char gender, Department department, double salary, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.department = department;
+        this.salary = salary;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
