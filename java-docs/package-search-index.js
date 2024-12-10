9f9b122e221e0fd89234ec148591e0cde6f1bf9e
Claus-Factor <volxin.k2004@gmail.com>
2024-12-10 17:45:18 +0300

    Generate javadoc

---
+++ java-docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.labs.csvparser.model"},{"l":"com.labs.csvparser.scvparser"}];updateSearchResults();
---
+++ java-docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.labs.csvparser.scvparser","l":"CsvParser"},{"p":"com.labs.csvparser.model","l":"Department"},{"p":"com.labs.csvparser.model","l":"Person"}];updateSearchResults();
